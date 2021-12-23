7701cfabd209898adba245f997b98a6e317205a6
s24279-pj <s24279@pjwstk.edu.pl>
2021-12-23 20:16:15 +0100

    poprawa zadanie 4, zajecia 5.

--- cwiczenia5/zadanie4/main.cpp
+++ cwiczenia5/zadanie4/main.cpp
@@ -3,36 +3,31 @@
 
 using namespace std;
 
-float cone(float r, float h)
+void cone(float r, float h, float &field, float &volume)
 {
-    float field=(M_PI)*r*r;
-    float volume=((field)*h)/3;
+    field=(M_PI)*r*r;
+    volume=((M_PI)*r*r*h)/3;
+}
+
+int main()
+{
+    float r, h, field, volume;
+    cout.precision(4);
+    std::cout << "Podaj promien stozka: ";
+    std::cin >> r;
+    std::cout << "Podaj wysokosc stozka: ";
+    std::cin >> h;
+    cone(r,h,field,volume);
     if (r>0 && h>0)
     {
     std::cout << "Pole stozka wynosi: " << field << endl;
     std::cout << "Objetosc stozka to: " << volume;
-    }
-    else if (r>0 && h==0)
+    }else if (r>0 && h==0)
     {
     std::cout << "Pole stozka wynosi: " << field << endl;
     std::cout << "Twoj stozek jest kolem.";
     }else
     std::cout << "Taki stozek nie istnieje";
-    return false;
-
-}
-
-
-int main()
-{
-    float r, h;
-    std::cout << "Podaj promien stozka: ";
-    std::cin >> r;
-    std::cout << "Podaj wysokosc stozka: ";
-    std::cin >> h;
-    std::cout << endl;
-    cout.precision(4);
-    std::cout << cone(r,h);
     std::cout << endl;
 
     return 0;
