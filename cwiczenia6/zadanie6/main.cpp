cb482fd9d1eac5b190d6f242d808e596cb52a3fe
s24279-pj <s24279@pjwstk.edu.pl>
2022-01-22 13:28:01 +0100

    ostatni update

--- cwiczenia6/zadanie6/main.cpp
+++ cwiczenia6/zadanie6/main.cpp
@@ -2,27 +2,28 @@
 
 using namespace std;
 
-int odwracanie(int tab[], int n)
+int* odwracanie(int tab[], int n)
 {
-    int i;
     for (int i=0; i<n; i++)
     {
     swap(tab[i],tab[n-1]);
     n--;
     }
-    return
+    return tab;
 }
 
 int main()
 {
     int n;
     int tab[10] = {1,2,3,4,5,6,7,5,4,3};
+
+
     cout << "Twoja tablica" << endl;
     for (int i=0; i<10; i++)
     {
         cout << tab[i] << " ";
     }
-    cout << "Podaj ile pierwszych elementow odwrocic:";
+    cout << "\nPodaj ile pierwszych elementow odwrocic:";
     cin >> n;
     odwracanie(tab,n);
     cout << "Twoja tablica po funkcji odwracanie" << endl;
@@ -31,11 +32,10 @@ int main()
         cout << tab[i] << " ";
     }
     odwracanie(odwracanie(tab,n),n);
-    cout << "Twoja tablica po odwracanie(odwracanie" << endl;
+    cout << "\nTwoja tablica po odwracanie(odwracanie,n)" << endl;
     for (int i=0; i<10; i++)
     {
         cout << tab[i] << " ";
     }
-
     return 0;
 }
